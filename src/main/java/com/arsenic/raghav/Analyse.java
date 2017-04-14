1e9c2ccc07942c3f85e6f1686572556e899598e8
arsenicraghav <mail.raghvendra.kumar@gmail.com>
2017-04-14 16:01:55 +0530

    removing the model training approach

--- src/main/java/com/arsenic/raghav/Analyse.java
+++ src/main/java/com/arsenic/raghav/Analyse.java
@@ -3,11 +3,9 @@ package com.arsenic.raghav;
 /**
  * Created by raghav on 14/04/17.
  */
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.*;
+
 import opennlp.tools.doccat.DoccatModel;
 import opennlp.tools.doccat.DocumentCategorizerME;
 import opennlp.tools.doccat.DocumentSampleStream;
@@ -24,17 +22,17 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class Analyse {
 
-    DoccatModel model;
-    static int positiveTweetCount = 0;
-    static int negativeTweetCount = 0;
+    static Map<String, Integer> lookupMap = new HashMap<String, Integer>();
 
     public static void main(String[] args) throws IOException, TwitterException {
 
         Analyse analyser = new Analyse();
 
-        /* Train the analyser first */
+        /* Load the Lookup in a HashMap first */
 
-        analyser.trainAnalyser();
+        LoadToHashmap();
+
+        System.out.println(lookupMap.size());
 
         /*
 
@@ -68,77 +66,120 @@ public class Analyse {
         TwitterFactory twitterFactory = new TwitterFactory(configurationBuilder.build());
         Twitter twitter = twitterFactory.getInstance();
 
-         /* query twitter for tweets */
+        BufferedWriter bw = new BufferedWriter(new FileWriter("/Users/raghav/Desktop/TwitterSentiment/results.csv"));
 
-        Query query = new Query("USA");
-        QueryResult result = twitter.search(query);
+        /* List of countries to compare */
 
+        List <String> countries =  new ArrayList<String>();
+
+        countries.add("USA");
+        countries.add("INDIA");
+        countries.add("CANADA");
+        countries.add("ENGLAND");
+        countries.add("KOREA");
+        countries.add("TAIWAN");
+        countries.add("RUSSIA");
+
+        Map<String, Integer> countryScore = new HashMap<String, Integer>();
+
+         /* query twitter for tweets for each country */
+
+        for(String country : countries) {
+
+            Query query = new Query(country);
+            QueryResult result = twitter.search(query);
+
+            int positiveTweetCount = 0;
+            int negativeTweetCount = 0;
 
         /* process each tweet from the search result */
 
-        for (Status status : result.getTweets()) {
+            for (Status status : result.getTweets()) {
 
-            if (analyser.checkPositiveOrNegtiveTweet(status.getText()) == 1) {
-                positiveTweetCount++;
-            } else {
-                negativeTweetCount++;
+                if (analyser.checkPositiveOrNegtiveTweet(status.getText()) == 1) {
+                    positiveTweetCount++;
+                } else {
+                    negativeTweetCount++;
+                }
             }
-        }
+
+            countryScore.put(country,negativeTweetCount);
 
         /* write the final count to the result.csv file */
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter("/Users/raghav/Desktop/TwitterSentiment/results.csv"));
-        bw.write("Positive Tweets," + positiveTweetCount);
-        bw.newLine();
-        bw.write("Negative Tweets," + negativeTweetCount);
+            bw.write("Country: " + country);
+            bw.newLine();
+            bw.write("Positive Tweets: " + positiveTweetCount);
+            bw.newLine();
+            bw.write("Negative Tweets: " + negativeTweetCount);
+            bw.newLine();
+            bw.newLine();
+        }
+
+        /* Let's assume Happiest Country is based on the minimum number of negative tweets(In case of a tie we declare the first one winner */
+
+        int minValueInMap  = Collections.min(countryScore.values());
+
+        for (Map.Entry<String, Integer> entry : countryScore.entrySet()) {
+
+            if (entry.getValue()==minValueInMap) {
+                bw.newLine();
+                bw.write("Happiest Country is: " + entry.getKey());
+                bw.newLine();
+                break;
+            }
+        }
+
         bw.close();
     }
 
-    public void trainAnalyser() {
-
-        InputStream inputStream = null;
-
-        try {
-            inputStream = new FileInputStream("/Users/raghav/Desktop/TwitterSentiment/lookupfile.txt");
-
-            ObjectStream lineStream = new PlainTextByLineStream(inputStream, "UTF-8");
-
-            ObjectStream sampleStream = new DocumentSampleStream(lineStream);
-
-            int cutoff = 2;
-            int trainingIterations = 20;
-
-            model = DocumentCategorizerME.train("en", sampleStream, cutoff, trainingIterations);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null) {
-                try{
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+    /* Function to check negative or positive tweet */
 
     public int checkPositiveOrNegtiveTweet(String tweet) throws IOException {
 
-        DocumentCategorizerME categorizer = new DocumentCategorizerME(model);
+        System.out.println("Checking category for the tweetWord:" + tweet + " ===> ");
 
-        double[] outcomes = categorizer.categorize(tweet);
-        String category = categorizer.getBestCategory(outcomes);
+        int tweetWeight = 0;
 
-        System.out.print("Checking category for the Tweet:" + tweet + " ===> ");
+        String tweetSplit[] = tweet.split("\\s+");
 
-        if (category.equalsIgnoreCase("1")) {
+        for (String tweetWord : tweetSplit) {
+
+            if(lookupMap.containsKey(tweetWord)) {
+
+                int weight = lookupMap.get(tweetWord) ;
+                tweetWeight = tweetWeight + weight ;
+            }
+        }
+
+        System.out.println(tweetWeight);
+
+        if (tweetWeight > 0) {
             System.out.println(" Positive Tweet ");
             return 1;
         } else {
             System.out.println(" Negative Tweet ");
             return 0;
         }
+    }
 
+    /* Function to load lookup file in a HashMap */
+
+    public static void LoadToHashmap() throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new FileReader("/Users/raghav/Desktop/TwitterSentiment/lookupfile.txt"));
+
+        while (scanner.hasNextLine())
+        {
+            String line = scanner.nextLine();
+            String lineSplit[] = line.split("\\s+");
+
+            try {
+                Integer value = Integer.parseInt(lineSplit[1]);
+                lookupMap.put(lineSplit[0], value);
+            }catch(Exception e){
+            }
+
+        }
     }
 }
